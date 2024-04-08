74911a401a75a30ed9a989662e242bfe1a1be7d9
Siya Nkuna <sudosprogrammer@gmail.com>
2024-04-08 20:22:53 +0200

    CCT-14 add convert script endpoint

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -26,7 +26,7 @@ namespace CodeConverterTool.Controllers
                 Method = Method.Post
             };
             request.AddHeader("Content-Type", "application/json");
-            string jsonBody = "{\"client_id\":\"" + "" + "\",\"client_secret\":\"" + Environment.GetEnvironmentVariable("CLIENT_SECRET") + "\",\"audience\":\"https://localhost:7074/swagger/index.html/api\",\"grant_type\":\"client_credentials\"}";
+            string jsonBody = "{\"client_id\":\"" + Environment.GetEnvironmentVariable("CLIENT_ID") + "\",\"client_secret\":\"" + Environment.GetEnvironmentVariable("CLIENT_SECRET") + "\",\"audience\":\"https://localhost:7074/swagger/index.html/api\",\"grant_type\":\"client_credentials\"}";
 
             request.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
 
---
+++ Controllers/ScriptConvertController.cs
@@ -0,0 +1,62 @@
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NuGet.Protocol;
+
+namespace CodeConverterTool.Controllers
+{
+    [ApiController]
+	[Route("[controller]")]
+	public class ScriptConvertController : ControllerBase
+	{
+
+		[Authorize]
+		[HttpPost("{apiKey}")]
+		public async Task<IActionResult> PostScriptConvert(string apiKey, Models.ScriptConvert scriptConvert)
+		{
+			try
+			{
+				var model = scriptConvert.Model;
+				var content = scriptConvert.Content;
+				var sourceScript = scriptConvert.SourceScript;
+				var targetScript = scriptConvert.TargetScript;
+				var maxTokens = scriptConvert.MaxTokens;
+
+				using HttpClient client = new();
+
+				client.BaseAddress = new Uri("https://api.openai.com/v1/");
+				client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+				var req = new
+				{
+                    model,
+					prompt = $"Translate the following {sourceScript} code to {targetScript}: {content}",
+					max_tokens = maxTokens
+				};
+
+				var jsonReq = new StringContent(JsonConvert.SerializeObject(req), Encoding.UTF8, "application/json");
+
+				var response = await client.PostAsync("completions", jsonReq);
+
+				if (response.IsSuccessStatusCode)
+				{
+					var jsonResponse = await response.Content.ReadAsStringAsync();
+
+					var parsedResponse = JObject.Parse(jsonResponse);
+
+					return Ok(parsedResponse["choices"][0]["text"].ToString());
+				}
+				else
+				{
+					return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+				}
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, ex.Message);
+			}
+		}
+	}
+}
